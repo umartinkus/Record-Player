7d56ca2cd8f1163c70f773f8c1eba4cf19072144
umartinkus <63037721+umartinkus@users.noreply.github.com>
2026-08-08 14:09:12 -0600

    creating PZ plot of cartridge TF

--- Mechanical and System/system_model.m
+++ Mechanical and System/system_model.m
@@ -4,6 +4,8 @@ L1 = 0.80e-3; C2 = 25e-6; C1 = 0.0506e-6; R = 40;
 fprintf('Series (force notch):  %.0f Hz\n', 1/(2*pi*sqrt(L1*C2)));
 fprintf('Parallel (force peak): %.0f Hz\n', 1/(2*pi*sqrt(L1*C1)));
 
+
 figure; bode(Hvel, Hforce, {2*pi*20, 2*pi*50e3});
+figure; pzplot(Hvel, Hforce);
 grid on;
 legend('Velocity response','Tracking force');
